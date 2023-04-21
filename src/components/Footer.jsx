7bb57064dfc7f19cf62e4dc9117e9cde1f510e50
fostersoasare <fostersoasare@gmail.com>
2023-04-21 12:15:23 +0000

    Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import Link from "next/link";
 import styles from "../styles/Footer.module.scss";
 
+import { TiSocialFacebookCircular, TiSocialTwitter } from "react-icons/ti";
+import { BsInstagram } from "react-icons/bs";
+
 const Footer = () => {
-	return <footer className={styles.footer}>Footer</footer>;
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.footer__container}>
+				<h3>Ritz Car Hire</h3>
+				<nav className={styles.footer__navigation}>
+					<Link href="/">Home</Link>
+					<Link href="/">Catalogue</Link>
+					<Link href="/">Hire</Link>
+					<Link href="/">Contact Us</Link>
+				</nav>
+				<div className={styles.footer__socials}>
+					<TiSocialFacebookCircular />
+					<BsInstagram />
+					<TiSocialTwitter />
+				</div>
+				<p>&#169; 2023 All rights reserved</p>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
 		<div className="App">
 			<Header />
 			<Component {...pageProps} />
-			{/* <Footer /> */}
+			<Footer />
 		</div>
 	);
 }
