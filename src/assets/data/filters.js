0e782bb9577d4adec1bc3ba26d1aedd5fb49903d
fostersoasare <fostersoasare@gmail.com>
2023-04-23 12:14:01 +0000

    Filter functionality

---
+++ src/assets/data/filters.js
@@ -0,0 +1,24 @@
+const filters = [
+	{
+		title: "Type",
+		options: ["Sedan/Saloon", "SUV", "Bus", "Pick-up", "Truck", "Van"],
+	},
+	{
+		title: "Transmission",
+		options: ["Manual", "Automatic"],
+	},
+	{
+		title: "Seats",
+		options: ["2 and above", "4 and above", "8 and above"],
+	},
+	{
+		title: "Doors",
+		options: ["2 Doors", "4 Doors"],
+	},
+	{
+		title: "Availability",
+		options: ["Currently Available", "Soon to be Available"],
+	},
+];
+
+export default filters;
--- src/pages/catalogue.js
+++ src/pages/catalogue.js
@@ -2,17 +2,59 @@ import React, { useState } from "react";
 import styles from "../styles/Catalogue.module.scss";
 
 import cars from "@/assets/data/cars";
+import filters from "@/assets/data/filters";
 
 import CarCard from "@/components/CarCard";
 const Catalogue = () => {
 	let [limit, setLimit] = useState(9);
+	let [filter, setFilter] = useState(
+		filters.map((e) => {
+			return { [e.title.toLowerCase()]: [] };
+		})
+	);
 	function loadMoreCars() {
 		setLimit((prev) => prev + 3);
+	}
+
+	function filterCars(e) {
+		e.preventDefault();
+		console.log(filter);
+	}
+
+	function toggleFilterOption(type, value) {
+		type = type.toLowerCase();
+		setFilter((prev) => {
+			return [
+				...prev.map((e) => {
+					return e[type] ? { ...e, [type]: e[type].includes(value) ? e[type].filter((option) => option !== value) : [...e[type], value] } : e;
+				}),
+			];
+		});
 	}
 	return (
 		<main className={styles.catalogue}>
 			<aside id={styles.filters}>
-				<h3>FILTERS</h3>
+				<h3 className={styles.catalogue__title}>FILTERS</h3>
+				<form onSubmit={filterCars}>
+					{filters.map((filter, index) => (
+						<div key={index} className={styles.catalogue__filter}>
+							<h3>{filter.title}</h3>
+							{filter.options.map((option, index) => (
+								<div className={styles.filter__option} key={index}>
+									<input
+										type="checkbox"
+										id={option.toLowerCase()}
+										name={filter.title.toLowerCase()}
+										// value={option.toLowerCase()}
+										onChange={() => toggleFilterOption(filter.title, option)}
+									/>
+									<label htmlFor={option.toLowerCase()}>{option}</label>
+								</div>
+							))}
+						</div>
+					))}
+					<button>Filter</button>
+				</form>
 			</aside>
 			<section id={styles.content}>
 				<h3 className={styles.content__title}>CATALOGUE</h3>
